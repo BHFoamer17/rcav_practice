f14f4e04ae2c2237a52885b3ac63c4489d88e8d9
BHFoamer17 <bharris1@chicagobooth.edu>
2015-05-10 15:14:41 -0500

    draft 1
    I have the square root function working properly.  The interest rate is
    not spitting out the same number the professor has.

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -3,8 +3,27 @@ class CalculationsController < ApplicationController
   end
 
   def sqrt
+    @number = params["num"]
+    @number1 = Math.sqrt(params["num"].to_i)
+    @square_root = @number1.round(2)
   end
 
   def pmt
+
+  @apr = params[:annual_percentage_rate].to_f
+  @years = params[:number_of_years].to_i
+  @principal = params[:principal_value].to_f
+
+
+  apr_decimal = @apr/100/12
+  months = @years*-12
+  num = apr_decimal*@principal
+  den = 1-((1+apr_decimal)**months)
+
+  @monthly_payment = num/den
+
+
   end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  get("/", { :controller => "calculations", :action => "instructions" })
+get("/", { :controller => "calculations", :action => "instructions" })
+
+
+get("/square_root/:num",   { :controller => "calculations", :action => "sqrt" })
+
+get("/payment/:annual_percentage_rate/:number_of_years/:principal_value", { :controller => "calculations", :action => "pmt" })
+
+
 end
